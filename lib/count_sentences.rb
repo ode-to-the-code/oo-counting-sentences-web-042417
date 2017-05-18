161fa0ab25d459266337c70a0a2b04d89340310f
ode-to-the-code <justcallmethis@gmail.com>
2017-05-18 09:55:59 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if  self.end_with?(".") then return true
+    else false
+  end
   end
 
   def question?
-
+    if self.end_with?("?") then return true
+    else false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    self.split(".", "?")
+    binding.pry
+    x.count
+    binding.pry
   end
 end
